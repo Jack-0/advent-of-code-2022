047ff92a3c60efabf16b3845846d7713659b4f1d
jack-0 <github.ay8yf@slmail.me>
2023-06-03 22:13:43 +0100

    Day 13: Part 2
    learning to read the problem :)

--- 2022/typescript/src/days/day13.ts
+++ 2022/typescript/src/days/day13.ts
@@ -11,15 +11,20 @@ export const day13 = (data: string[]): Solution => {
   const f = data.filter((x) => x !== "");
   let idx = 1;
   const pairs: Pair[] = [];
+
+  const allLines = [];
   for (let i = 0; i < f.length; i += 2) {
     let left = JSON.parse(f[i]);
     let right = JSON.parse(f[i + 1]);
     pairs.push({ left, right, idx, correct: false });
     idx++;
+    allLines.push(left);
+    allLines.push(right);
   }
   enum Result {
+    loop = 0,
     correct = 1,
-    notCorrect = -1,
+    notCorrect = -1
   }
   function compareLR(left: any, right: any, idx = -1): Result {
     left = structuredClone(left);
@@ -34,24 +39,21 @@ export const day13 = (data: string[]): Solution => {
       }
       return 0;
     } else if (!Array.isArray(left) && !Array.isArray(right)) {
-      //numbers
       return Math.sign(left - right);
     } else {
-      // mixed
       if (Array.isArray(left)) {
         return compareLR(left, [right]);
       }
       return compareLR([left], right);
     }
   }
+
   pairs.forEach((p) => {
     p.correct =
       compareLR(p.left, p.right, p.idx) !== Result.correct ? true : false;
   });
+
   function part1() {
-    pairs.forEach((p) => {
-      console.log(p);
-    });
     return pairs
       .map((p) => (p.correct === true ? p.idx : 0))
       .reduce((a, b) => {
@@ -59,9 +61,19 @@ export const day13 = (data: string[]): Solution => {
       }, 0);
   }
   function part2() {
-    // const left = JSON.parse("[[2]]");
-    // const right = JSON.parse("[[6]]");
-    // pairs.push({ left, right, idx, correct: false });
+    const sorted = allLines.sort((a, b) => {
+      return compareLR(a, b);
+    });
+
+    let twoPos = 0;
+    let sixPos = 0;
+    sorted.map((x, i) => {
+      if (x.length === 0) return;
+      if (compareLR(x, [[2]]) === 1 && twoPos === 0) twoPos = i + 1;
+      if (compareLR(x, [[6]]) === 1 && sixPos === 0) sixPos = i + 2;
+    });
+    // console.log(twoPos, sixPos);
+    return twoPos * sixPos;
   }
 
   return { part1, part2 };
