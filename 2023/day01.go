f3c94e6da01a7994c08d661c54cfbe876b5642c8
Jack-0 <github.ay8yf@slmail.me>
2023-12-01 21:17:09 +0000

    golang day01 part 2

--- 2023/day01.go
+++ 2023/day01.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-        "strconv"
-        "regexp"
+	"regexp"
+	"strconv"
+	"strings"
+	"sort"
 )
 
 func eachLine(line string){
@@ -35,9 +37,51 @@ func concatenateNumbers(nums []int) int {
 	return result
 }
 
+func convertWordsToInt(input string) string {
+	wordToValue := map[string]int{
+		"zero":  0,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+	type IndexValue struct {
+		Index   int
+		Value int
+	}
+	toAppend := []IndexValue{}
+	
+	for word, value := range wordToValue {
+		re := regexp.MustCompile(word)
+
+		indices := re.FindAllStringIndex(input, -1)
+		for _, indexPair := range indices {
+			_, endIndex := indexPair[0], indexPair[1]
+			toAppend = append(toAppend, IndexValue{endIndex, value})
+		}
+	}		
+
+	sort.SliceStable(toAppend, func(i,j int) bool {
+		return toAppend[i].Index < toAppend[j].Index
+	})
+
+	index_offset := 0
+	for _, pair := range toAppend {
+		input = input[:pair.Index+index_offset] + strconv.Itoa(pair.Value) + input[pair.Index + index_offset:]
+		index_offset += 1
+	}
+
+	return input
+}
 
 
 func main() {
+    // file, err := os.Open("day01_example_p2.txt")
     // file, err := os.Open("day01_example.txt")
     file, err := os.Open("day01.txt")
 
@@ -54,13 +98,14 @@ func main() {
 
     for scanner.Scan() {
         line := scanner.Text()
-        // eachLine(line)
-        values := extractNumbers(line)
+	lowercase_line := strings.ToLower(line)
+	converted_line := convertWordsToInt(lowercase_line)
+        values := extractNumbers(converted_line)
         first := values[0]
 	last := values[len(values)-1]
         value := concatenateNumbers([]int{first,last})
         total += value
     }
 
-    fmt.Println(total)
+    fmt.Println("total", total)
 }
