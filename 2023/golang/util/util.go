25b341439bfa3586b7470e275e2dbf868262365b
Jack-0 <github.ay8yf@slmail.me>
2023-12-07 22:34:55 +0000

    var name change

--- 2023/golang/util/util.go
+++ 2023/golang/util/util.go
@@ -8,8 +8,8 @@ import (
 func DataTo2DArray(s *bufio.Scanner) (arr [][]string) {
 	for s.Scan() {
 		temp := make([]string, 0)
-		for _, v := range strings.Split(s.Text(), "") {
-			temp = append(temp, v)
+		for _, x := range strings.Split(s.Text(), "") {
+			temp = append(temp, x)
 		}
 		arr = append(arr, temp)
 	}
