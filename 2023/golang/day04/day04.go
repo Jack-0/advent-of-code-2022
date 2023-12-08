32e6aee325cc252fca9a0d3d22c8056b3785f36e
Jack-0 <github.ay8yf@slmail.me>
2023-12-08 19:22:47 +0000

    golang day04 part 2

--- 2023/golang/day04/day04.go
+++ 2023/golang/day04/day04.go
@@ -2,48 +2,78 @@ package day04
 
 import (
 	"bufio"
-	"fmt"
-	"strconv"
 	"strings"
 )
-
-func stringToNumArr(str []string) []int {
-	var numbers []int
-	for _, s := range str {
-		n, err := strconv.Atoi(s)
-		if err == nil {
-			numbers = append(numbers, n)
-		}
-	}
-	return numbers
-}
 
 func Part1(s *bufio.Scanner) int {
 	total := 0
 	for s.Scan() {
-		line := s.Text()
-		// unclean way of organising the data
-		winningCards := stringToNumArr(strings.Split(strings.Split(strings.Split(line, ":")[1], "|")[0], " "))
-		myCards := stringToNumArr(strings.Split(strings.Split(line, "|")[1], " "))
+		decks := strings.Split(strings.Split(s.Text(), ":")[1], "|")
+		winningCards := strings.Fields(decks[0])
+		myCards := strings.Fields(decks[1])
+		myCardMap := make(map[string]int)
+		for _, x := range myCards {
+			myCardMap[x]++
+		}
 
 		roundTotal := 0
-		for _, mc := range myCards {
-			for _, wc := range winningCards {
-				if mc == wc {
-					if roundTotal == 0 {
-						roundTotal = 1
-					} else {
-						roundTotal = roundTotal * 2
-					}
+		for _, x := range winningCards {
+			value, exists := myCardMap[x]
+
+			if exists && value > 0 {
+				// roundTotal++
+				if roundTotal == 0 {
+					roundTotal = 1
+				} else {
+					roundTotal *= 2
 				}
+				myCardMap[x]--
 			}
 		}
 		total += roundTotal
-		fmt.Println(winningCards, myCards, roundTotal)
 	}
+
 	return total
 }
 
 func Part2(s *bufio.Scanner) int {
-	return -1
+	total := 0
+	lineNum := 1
+
+	scratchCards := map[int]int{}
+
+	for s.Scan() {
+		scratchCards[lineNum] += 1
+
+		decks := strings.Split(strings.Split(s.Text(), ":")[1], "|")
+		winningCards := strings.Fields(decks[0])
+		myCards := strings.Fields(decks[1])
+		
+		myCardMap := make(map[string]int)
+		for _, x := range myCards {
+			myCardMap[x]++
+		}
+
+		roundTotal := 0
+		for _, x := range winningCards {
+			value, exists := myCardMap[x]
+			if exists && value > 0 {
+				roundTotal++
+				myCardMap[x]--
+			}
+		}
+
+		for i := 1; i <= roundTotal; i++ {
+			scratchCards[lineNum+i] += scratchCards[lineNum]
+		}
+		
+		lineNum++
+	}
+
+	for _, x := range scratchCards {
+		if x > 0 {
+			total += x
+		}
+	}
+	return total
 }
